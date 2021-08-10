0dcf215685511b804471127dd0daf704ab7fa2b8
Matthew Thompson <thompson.m.j@outlook.com>
2021-08-10 16:57:41 -0400

    eod

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -3,6 +3,7 @@ module MyExample
 # Write your package code here.
 include("extra_file.jl")
 
+
 export my_f # from extra_file.jl
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using MyExample
 using Test
 
+MyExample.my_f(2,1)
+
 @testset "MyExample.jl" begin
     # Write your tests here.
 end
