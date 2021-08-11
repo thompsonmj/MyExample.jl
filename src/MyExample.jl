eace9fda03c3b2ca6170aff63bf8c4d1fae8aef1
Matthew Thompson <thompson.m.j@outlook.com>
2021-08-11 13:24:15 -0400

    testsets

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,9 +1,12 @@
 module MyExample
 
+using ForwardDiff
+
 # Write your package code here.
 include("extra_file.jl")
 
 
 export my_f # from extra_file.jl
+export derivative_of_my_f
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,2 @@
-using MyExample
-using Test
-
-@testset "MyExample.jl" begin
-
-    @test my_f(2,1) == 7
-    @test my_f(2,3) == 13
-    @test my_f(1,1) == 5
-
-end
+using SafeTestsets
+@safetestset "My f tests" begin include("my_f_tests.jl") end
